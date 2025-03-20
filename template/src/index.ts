bdca5e427dae616b72070af1dc3a54aba614dcb5
Manoj Yadav <165868833+manojyadav7@users.noreply.github.com>
2025-03-20 12:16:48 +0530

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,9 +3,6 @@ import { R2Explorer } from "r2-explorer";
 // Read all available configurations here: https://r2explorer.dev/getting-started/configuration/
 export default R2Explorer({ 
   readonly: false, 
-  basicAuth: {
-    username: 'user@terradx.ai',
-    password: 'password@terradx.ai'
-  },
+  cfAccessTeamName: 'terradx' 
 });
 
