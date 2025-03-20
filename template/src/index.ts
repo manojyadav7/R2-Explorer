29fee174f5600e97431855faebfa754824ab37d1
Manoj Yadav <165868833+manojyadav7@users.noreply.github.com>
2025-03-20 11:40:33 +0530

    fixed linter error

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,8 +3,8 @@ import { R2Explorer } from "r2-explorer";
 // Read all available configurations here: https://r2explorer.dev/getting-started/configuration/
 export default R2Explorer({
 	readonly: true,
-	basicAuth: {
-	  username: 'user@terradx.ai',
-	  password: 'password@terradx.ai'
-	}
+  basicAuth: {
+    username: "user@terradx.ai",
+    password: "password@terradx.ai",
+  },
 });
