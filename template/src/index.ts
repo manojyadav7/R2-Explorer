8dc00689c751a15edf1e5f50425af677e780eb84
Jatin Kumar <97216852+JATINKUMAR1008@users.noreply.github.com>
2025-03-20 12:48:46 +0530

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,9 +1,3 @@
-import { R2Explorer } from "r2-explorer";
+import { R2Explorer } from 'r2-explorer';
 
-// Read all available configurations here: https://r2explorer.dev/getting-started/configuration/
-export default R2Explorer({
-  readonly: false,
-  cfAccessTeamName: "terradx",
-});
-
-
+export default R2Explorer({ readonly: false, cfAccessTeamName: 'terradx' });
