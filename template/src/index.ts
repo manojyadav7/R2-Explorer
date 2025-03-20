def7abb7829d1b736f343a2d8c2788f20cc0da53
Manoj Yadav <165868833+manojyadav7@users.noreply.github.com>
2025-03-20 12:20:34 +0530

    team access

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,8 +1,9 @@
 import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.dev/getting-started/configuration/
-export default R2Explorer({ 
-  readonly: false, 
-  cfAccessTeamName: 'terradx' 
+export default R2Explorer({
+  readonly: false,
+  cfAccessTeamName: "terradx",
 });
+
 
